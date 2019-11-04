1ef662cff874b5ecba1ae8f1f34a1551ff9f6bd9
swathivadlakonda <52874669+swathivadlakonda@users.noreply.github.com>
2019-11-04 06:59:00 +0530

    added

---
+++ hello.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo hello world123... > /var/www/html/1.html
+echo welcome to ansible > /var/www/html/1.html
+~                                             
+
+
+for checking output...
+   ipaddress/1.html{filename}
+   
+   for more clarification please visit this website
+   https://www.youtube.com/watch?v=YI48bKykx7k[ansible-playbook]
+   
